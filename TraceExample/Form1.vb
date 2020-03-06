48a631f242aabea10841cc8e0a0ec0545d4ea077
Kusyun <kusyunexcl@gmail.com>
2020-03-06 14:40:01 +0800

    加入專案檔案。

---
+++ TraceExample/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim stacktrace As New StackTrace(True)
+        Trace.WriteLine("目前行號: " + StackTrace.GetFrame(0).GetFileLineNumber().ToString())
+        Trace.WriteLine("目前方法: " + StackTrace.GetFrame(0).GetMethod().Name)
+        Trace.WriteLine("呼叫名稱: " + stacktrace.GetFrame(1).GetMethod().Name)
+
+        Debug.WriteLine("Debug 紀錄")
+        Trace.WriteLine("Trace 紀錄")
+    End Sub
+End Class
